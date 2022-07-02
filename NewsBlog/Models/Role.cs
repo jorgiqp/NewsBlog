a4b497b2bfa90a7d4558384cc57d1acecece2f1f
Igor Chernov <igorek.chernov007@gmail.com>
2022-07-02 14:48:53 +0300

    Додано 2 моделі таблиці. Завантажено пакет EntityFramework

---
+++ NewsBlog/Models/Role.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace NewsBlog.Models
+{
+    public class Role
+    {
+        //може бути три ролі "Admin", "Editor", "Client". Внесемо в базу,
+        //використовуємо для динамічного додавання кожному клієнту
+        [Key]
+        public int RoleId { get; set; }
+        public string Name { get; set; }
+
+        public Role()
+        {
+
+        }
+    }
+}
---
+++ NewsBlog/Models/User.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace NewsBlog.Models
+{
+    public class User
+    {
+        [Key] //треба для підключення таблиць між собою за допомогою "Id"
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public string? Surname { get; set; }
+        public string Email { get; set; }
+        public string? Comments { get; set; }
+        public string Role { get; set; } //вносимо роль клієнта може бути "Admin", "Editor", "Client"
+
+        public User()
+        {
+
+        }
+
+    }
+}
